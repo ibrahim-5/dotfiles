1293810ca52b0325015e3893c862c5567817e622
Ibrahim Hassan <ibrahimh2002x@gmail.com>
2024-04-09 00:21:42 +0100

    Fish Config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,16 @@
 local wezterm = require('wezterm')
 local config = wezterm.config_builder()
+
+-- Theme
 config.color_scheme = 'Gruvbox Dark (Gogh)'
+
+-- Disable Window Border
+config.window_decorations = "RESIZE"
+
+-- Configure default shell to Fish
+config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
+
+-- Set Opacity - Kills Lag on Macbook M2
+config.window_background_opacity = 0.9999
 
 return config
